4c272f7e36aa268bb1b28501a3ab39d8c33989f5
Shaik Rahuman <103171089+shaikdev@users.noreply.github.com>
2023-09-22 15:06:06 +0530

    file create and read

---
+++ 18files/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+
+	const content string = "Hello this is the testing for golang file creation"
+	file, err := os.Create("./mygolangfile.text")
+	checkErrorNil(err)
+	io.WriteString(file, content)
+	file.Close()
+	readFile("./mygolangfile.text")
+
+}
+
+func readFile(path string) {
+	readFile, err := ioutil.ReadFile(path)
+	checkErrorNil(err)
+	fmt.Println("The file content is: ", string(readFile))
+}
+
+func checkErrorNil(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
